9d14d570580be5b19787d8490eba144c59d21516
NisrinaAulia <31603046+NisrinaAulia@users.noreply.github.com>
2017-10-16 17:39:06 +0700

    FORM INSTAGRAM
    NIISRINA AULIA SHOLEHAH(26)
    XI RPL 2
    SMK TELKOM PURWOKERTO

---
+++ instagram/hasil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$host= "localhost";
+$user= "root";
+$password= "";
+$Email=$_POST['Email'];
+$fullname=$_POST['fullname'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+?>
+Email : <?php echo"$Email" ?><br>
+fullname : <?php echo"$fullname" ?><br>
+username: <?php echo"$username" ?><br>
+password : <?php echo"$password" ?><br>
+</body>
+</html>
